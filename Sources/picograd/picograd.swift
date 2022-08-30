44325efe5afd3550a04cf85a3dd7440c4e6fec96
Victor Guerra <vm.guerramoran@criteo.com>
2022-08-30 22:37:30 +0200

    Computing grad for sum and mul operations.
    A complete definition of the `Value` class that wraps
    a `FloatingPoint` literal. As well, we introduce operations
    for sum and mul, including their forward and backward passes.
    Those binary operations support as well one of either arguments
    to be a `FloatingPoint` literail, for instane, sum:
    
    ```swift
    let l = 3.0 + Value(2.0)
    ```
    
    We introduce as well a basic suite but complete suit of tests.

--- Sources/picograd/picograd.swift
+++ Sources/picograd/picograd.swift
@@ -1,6 +1,119 @@
-public struct picograd {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+// MARK: Sum operations on `Value`
+
+/// Forward and backward pass for `+` operation
+func +<T>(lhs: Value<T>, rhs: Value<T>) -> Value<T> where T: FloatingPoint {
+    // Forward pass
+    let sumValue = Value(lhs.data + rhs.data, inputs: [lhs, rhs], op: "+")
+
+    // Backward pass
+    sumValue._backward = {
+        lhs.grad += sumValue.grad
+        rhs.grad += sumValue.grad
+    }
+    return sumValue
+}
+
+func +<T>(lhs: T, rhs: Value<T>) -> Value<T> where T: FloatingPoint {
+    let lhsValue = Value(lhs)
+    return lhsValue + rhs
+}
+
+func +<T>(lhs: Value<T>, rhs: T) -> Value<T> where T: FloatingPoint {
+    let rhsValue = Value(rhs)
+    return lhs + rhsValue
+}
+
+// MARK: Mul operations on `Value`
+
+/// Forward and backward pass for `-` operation
+
+func *<T>(lhs: Value<T>, rhs: Value<T>) -> Value<T> where T: FloatingPoint {
+    // Forward pass
+    let mulValue = Value(lhs.data * rhs.data, inputs: [lhs, rhs], op: "*")
+
+    // Backward pass
+    mulValue._backward = {
+        lhs.grad += rhs.data * mulValue.grad
+        rhs.grad += lhs.data * mulValue.grad
+    }
+    return mulValue
+}
+
+func *<T>(lhs: T, rhs: Value<T>) -> Value<T> where T: FloatingPoint {
+        // Forward pass
+    let lhsValue = Value(lhs)
+    return lhsValue * rhs;
+}
+
+func *<T>(lhs: Value<T>, rhs: T) -> Value<T> where T: FloatingPoint {
+        // Forward pass
+    let rhsValue = Value(rhs)
+    return lhs * rhsValue;
+}
+
+
+// MARK: `Value` definition
+class Value<T: FloatingPoint> : CustomStringConvertible {
+    /// String representation of `Value`
+    var description: String {
+        return "Value(data=\(self.data), grad=\(self.grad), label=\(self.label))"
+    }
+
+    /// Pretty name
+    let label: String
+
+    /// Underlaying data
+    let data: T
+
+    /// Computed gradient
+    var grad: T = T.zero
+
+    /// Operation that triggered the generation of this value
+    let op: String
+
+    /// Set of `Value` objects that contribute to compute `self`
+    let inputs: Set<Value>
+
+    /// Closure that computes the gradient
+    var _backward: () -> Void
+
+    init(_ data: T, label: String = "", inputs: Set<Value> = [], op: String = "") {
+        self.data = data
+        self.label = label
+        self.op = op
+        self.inputs = inputs
+        self._backward = {}
+    }
+
+    /// Triggers backward propagation of gradients using as origin node `self`
+    func backward() -> Void {
+        var topo = Array<Value>()
+        var visited = Set<Value>()
+
+        // Compute topological sort on the graph that leads to this `Value`
+        func build_topo(value: Value) {
+            guard !visited.contains(value) else { return }
+            visited.update(with: value)
+            value.inputs.forEach { build_topo(value: $0) }
+            topo.append(value)
+        }
+        build_topo(value: self)
+
+        self.grad = 1
+        topo.forEach { $0._backward() }
+    }
+}
+
+// MARK: Extentions on `Value`
+extension Value: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
+
+extension Value: Equatable {
+    static func == (lhs: Value<T>, rhs: Value<T>) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
     }
 }
--- Tests/picogradTests/picogradTests.swift
+++ Tests/picogradTests/picogradTests.swift
@@ -1,11 +1,48 @@
 import XCTest
+import Numerics
 @testable import picograd
 
 final class picogradTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(picograd().text, "Hello, World!")
+    func testSumBackward() throws {
+        let a = Value(2.0, label: "a")
+        let b = Value(3.0, label: "b")
+
+        let l = a + b
+        l.backward()
+
+        assert(l.grad.isApproximatelyEqual(to: 1.0))
+        assert(l.data.isApproximatelyEqual(to: 5.0), "`l.data(\(l.data)` shuold be 5.0")
+        assert(a.grad.isApproximatelyEqual(to: 1.0), "`a.grad(\(a.grad)` should be 1.0 ")
+        assert(b.grad.isApproximatelyEqual(to: 1.0), "`b.grad(\(a.grad)` should be 1.0 ")
     }
+
+    func testMulBackward() throws {
+        let a = Value(2.0, label: "a")
+        let b = Value(3.0, label: "b")
+        let l = a * b
+
+        l.backward()
+
+        assert(l.grad.isApproximatelyEqual(to: 1.0))
+        assert(l.data.isApproximatelyEqual(to: 6.0), "`l.data(\(l.data)` shuold be 5.0")
+        assert(a.grad.isApproximatelyEqual(to: b.data), "`a.grad(\(a.grad)` should be 1.0 ")
+        assert(b.grad.isApproximatelyEqual(to: a.data), "`b.grad(\(a.grad)` should be 1.0 ")
+    }
+
+    func testOneValueOperandInSum() throws {
+        let a = 3.0 + Value(2.0)
+        let b = Value(2.0) + 3.0
+
+        assert(a.data.isApproximatelyEqual(to: 5.0))
+        assert(b.data.isApproximatelyEqual(to: 5.0))
+    }
+
+    func testOneValueOperandInMul() throws {
+        let a = 3.0 * Value(2.0)
+        let b = Value(2.0) * 3.0
+
+        assert(a.data.isApproximatelyEqual(to: 6.0))
+        assert(b.data.isApproximatelyEqual(to: 6.0))
+    }
+
 }
